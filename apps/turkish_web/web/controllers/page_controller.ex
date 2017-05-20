f8a3d7df72bd0681813a4ab6392496f6d811d198
PG <pinargazel@googlemail.com>
2017-05-20 12:37:15 +0200

    WIP

--- apps/turkish_web/web/controllers/page_controller.ex
+++ apps/turkish_web/web/controllers/page_controller.ex
@@ -4,4 +4,13 @@ defmodule TurkishWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def decline(conn, %{"noun" => noun}) do
+    conn
+    |> assign(:nominative, noun)
+    |> assign(:accusative, Turkish.noun_to_accusative(noun))
+    |> assign(:dative, Turkish.noun_to_dative(noun))
+    |> assign(:locative, Turkish.noun_to_locative(noun))
+    |> render("index.html")
+  end
 end
--- apps/turkish_web/web/router.ex
+++ apps/turkish_web/web/router.ex
@@ -5,7 +5,6 @@ defmodule TurkishWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -17,6 +16,7 @@ defmodule TurkishWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/decline", PageController, :decline
   end
 
   # Other scopes may use custom stacks.
